0d813bbc8960675bafdda2c8a0c0c27e36e8a35a
fluffyapplemonsterparrotpickle <hershelthoma@gmail.com>
2023-02-19 23:40:36 -0600

    My First Web App with authentication and sql

---
+++ projects/auth.py
@@ -0,0 +1,63 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
+
+auth = Blueprint('auth', __name__,)
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    #login code goes here
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if the user actually exists
+    # take the user-supplied passowrd, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist  or password is incorrect, reload the page
+    
+    #if checked above passes, then we know user has right credentials
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
+
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    #code to validate and add user to database goes here
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+    age = request.form.get("age")
+
+    user = User.query.filter_by(email=email).first() #if this returns a user than email already exists in database
+    
+    if user: #if user is found we want to redirect back to signuppage so user can try again
+        flash('Email adress already exists man (or women)')
+        return redirect(url_for('auth.signup'))
+    
+    # create a new user with the form data. hash the password so the plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'), age=age)
+    
+    #adds new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged Out')
+    return redirect(url_for('main.index'))
